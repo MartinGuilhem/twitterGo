3688604d99ec0c2ac1cbbcab0dd1fb88dbf49289
Martin Guilhem <mjguilhem@circutor.com>
2023-08-09 16:11:37 +0200

    add user model

---
+++ models/user.go
@@ -0,0 +1,20 @@
+package models
+
+import (
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"time"
+)
+
+type User struct {
+	ID        primitive.ObjectID `bson:"_id,omitempty" json:"id"`
+	Name      string             `bson:"name" json:"name,omitempty"`
+	Surname   string             `bson:"surname" json:"surname,omitempty"`
+	Birthday  time.Time          `bson:"Birthday" json:"Birthday,omitempty"`
+	Email     string             `bson:"email" json:"email,omitempty"`
+	Password  string             `bson:"password" json:"password,omitempty"`
+	Avatar    string             `bson:"avatar" json:"avatar,omitempty"`
+	Banner    string             `bson:"banner" json:"banner,omitempty"`
+	Biography string             `bson:"biography" json:"biography,omitempty"`
+	Location  string             `bson:"location" json:"location,omitempty"`
+	WebSite   string             `bson:"website" json:"website,omitempty"`
+}
